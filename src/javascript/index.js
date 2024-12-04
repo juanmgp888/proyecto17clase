4d7dbcf4f940476dd132aa57a5b06df3081d0fb4
curso00 <curso00@nomail.com>
2024-12-04 13:54:17 +0100

    Ejemplo de como añadir listener a un control con javascript a lo bestia

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,5 @@
-console.log('Hola mundo... por segunda vez');
+document.getElementById('boton').onclick = function() {
+    alert('Me ha presionado... me voy de vacaciones a relajarme');
+}
+
+console.log('Hola mundo... por tercera vez');
