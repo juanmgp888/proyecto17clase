3e546aae570ffe0b09d5774aba3a39050a941c32
curso00 <curso00@nomail.com>
2024-12-05 13:54:13 +0100

    Mostramos la sección con el id correcto

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
-    solicitud = {
+    let solicitudes = [{
         "id": 1, 
         "nombre": "Juan",
         "apellido": "Secreto"
-    };
-
-
-    let solicitudes = [solicitud, solicitud, solicitud];
+    }, {
+        "id": 2, 
+        "nombre": "Antonio",
+        "apellido": "Pero"
+    }, {
+        "id": 3, 
+        "nombre": "de la Encarnación",
+        "apellido": "No tanto"
+    }];
 
 
     for (i = 0 ; i < solicitudes.length; i++ ) {
         $("#maestro").append(
-            $("<li>").text(solicitudes[i].nombre + ' ' + solicitudes[i].apellido)
+            $("<li>")
+                .text(solicitudes[i].nombre + ' ' + solicitudes[i].apellido)
+                .val(solicitudes[i])
+                .attr("id", "id" + solicitudes[i].id)
         );
     }
+
+
+
+    $("li").on("click", function(event) {
+        if ($("#detalle").is(':visible')) {
+            $("#detalle").hide();
+        } else {
+            $("#detalle").show();
+
+            let solicitud = $(this).attr("id");
+
+            $("#id").val(solicitud);
+            $("#nombre").val("Juan"  + solicitud);
+            $("#apellido").val("Secreto" + solicitud);            
+        }
+        
+    })
 });
