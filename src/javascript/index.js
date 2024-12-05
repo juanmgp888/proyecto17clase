4581ff673f914011a025c4f4c0dc68ad40541ffb
curso00 <curso00@nomail.com>
2024-12-05 13:01:28 +0100

    Ya montamos con datos

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-    let solicitudes = ["Mensaje1", "Hola", "Adios"];
+    solicitud = {
+        "id": 1, 
+        "nombre": "Juan",
+        "apellido": "Secreto"
+    };
+
+
+    let solicitudes = [solicitud, solicitud, solicitud];
+
 
     for (i = 0 ; i < solicitudes.length; i++ ) {
         $("#maestro").append(
-            $("<li>").text(solicitudes[i])
+            $("<li>").text(solicitudes[i].nombre + ' ' + solicitudes[i].apellido)
         );
     }
 });
