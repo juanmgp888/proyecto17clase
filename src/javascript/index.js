d9518939c0062bd9beb0d7045ee51a460b48fecd
curso00 <curso00@nomail.com>
2024-12-10 09:16:39 +0100

    Peticiones get a servidor, y su propio juego de datos

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,42 +1,17 @@
-$(document).ready(function() {
-    let solicitudes = [{
-        "id": 1, 
-        "nombre": "Juan",
-        "apellido": "Secreto"
-    }, {
-        "id": 2, 
-        "nombre": "Antonio",
-        "apellido": "Pero"
-    }, {
-        "id": 3, 
-        "nombre": "de la Encarnación",
-        "apellido": "No tanto"
-    }];
-
-
-    for (i = 0 ; i < solicitudes.length; i++ ) {
-        $("#maestro").append(
-            $("<li>")
-                .text(solicitudes[i].nombre + ' ' + solicitudes[i].apellido)
-                .val(solicitudes[i])
-                .attr("id", "id" + solicitudes[i].id)
-        );
-    }
-
-
-
-    $("li").on("click", function(event) {
-        if ($("#detalle").is(':visible')) {
-            $("#detalle").hide();
-        } else {
-            $("#detalle").show();
-
-            let solicitud = $(this).attr("id");
-
-            $("#id").val(solicitud);
-            $("#nombre").val("Juan"  + solicitud);
-            $("#apellido").val("Secreto" + solicitud);            
-        }
+$(function() {
+    console.log('Paso por aquí');
+    $("#listar").on("click", function() {
+        $.get("https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes", function(data) {
+            $("#resListar").text("Ok");
+            console.log(data);
+        })
+        
+    });
+    $("#leer").on("click", function() {
+        $.get("https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/1", function(data) {
+            $("#resLeer").text("Ok"); 
+            console.log(data);
+        })
         
     })
 });
